03658f93c18ea780850ee0e433cfcc81da64cc96
Hasan <97510148+hasanrizvi-hr@users.noreply.github.com>
2024-01-21 04:35:26 +0500

    Lines in moving section are been modifies as per the requirement

--- MovingPlatform.cpp
+++ MovingPlatform.cpp
@@ -1,6 +1,4 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "MovingPlatform.h"
 
 // Sets default values
@@ -8,7 +6,6 @@ AMovingPlatform::AMovingPlatform()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
