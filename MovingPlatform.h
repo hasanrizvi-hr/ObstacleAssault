f0fffea5a549e38a1d0179782ff39281f7779762
Hasan <97510148+hasanrizvi-hr@users.noreply.github.com>
2024-02-05 19:25:01 +0500

    mavingplatform.h is synced with movingplatform.cpp

--- MovingPlatform.h
+++ MovingPlatform.h
@@ -14,26 +14,20 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
 private:
-
 	UPROPERTY(EditAnywhere, Category="Moving Platform")
 	FVector PlatformVelocity = FVector(0,0,500);
 	UPROPERTY(EditAnywhere, Category="Moving Platform")
 	float MovedDistance = 500;
 	UPROPERTY(EditAnywhere, Category="Rotation")
 	FRotator RotationVelocity;
-	
 	FVector StartLocation;
-
 	void MovePlatform(float DeltaTime);
 	void RotatePlatform(float DeltaTime);
-
 	bool ShouldPlatformReturn() const;
 	float GetDistanceMoved() const; 
-
 };
+# moving platform.h is synced with moving platform.cpp
