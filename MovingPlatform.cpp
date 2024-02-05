593f2c793d93f5a925549d3b3a208ce75060d553
Hasan <97510148+hasanrizvi-hr@users.noreply.github.com>
2024-02-05 19:27:37 +0500

    both files are checked and verified

--- MovingPlatform.cpp
+++ MovingPlatform.cpp
@@ -1,27 +1,20 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "MovingPlatform.h"
-
 // Sets default values
 AMovingPlatform::AMovingPlatform()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 }
-
 // Called when the game starts or when spawned
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
-
 	StartLocation = GetActorLocation();
-
 	FString Name = GetName();
-
 	UE_LOG(LogTemp, Display, TEXT("Begin Play: %s"), *Name);
-
 	//SetActorLocation(MyVector);
 }
-
 // Called every frame
 void AMovingPlatform::Tick(float DeltaTime)
 {
@@ -29,7 +22,6 @@ void AMovingPlatform::Tick(float DeltaTime)
 	MovePlatform(DeltaTime);
 	RotatePlatform(DeltaTime);
 }
-
 void AMovingPlatform::MovePlatform(float DeltaTime)
 {
 	if (ShouldPlatformReturn())
@@ -55,11 +47,9 @@ void AMovingPlatform::RotatePlatform(float DeltaTime)
 bool AMovingPlatform::ShouldPlatformReturn() const 
 {
 	return GetDistanceMoved() > MovedDistance;
-
 }
-
 float AMovingPlatform::GetDistanceMoved() const
 {
 	return FVector::Dist(StartLocation, GetActorLocation());
-
 }
+#moving platform.cpp and moving platform.h are checked and verified twice
