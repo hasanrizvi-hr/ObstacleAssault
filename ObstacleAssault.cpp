cd2aa9cc56e060dd172507fe88e653c72462f08f
Hasan <97510148+hasanrizvi-hr@users.noreply.github.com>
2024-02-10 16:33:52 +0500

    Update ObstacleAssault.cpp

--- ObstacleAssault.cpp
+++ ObstacleAssault.cpp
@@ -2,3 +2,4 @@
 #include "ObstacleAssault.h"
 #include "Modules/ModuleManager.h"
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ObstacleAssault, "ObstacleAssault" );
+//parallel programming not implemented
