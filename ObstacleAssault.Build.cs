fca3d93f2b3737d88d54bec7e15e23408a351619
Hasan <97510148+hasanrizvi-hr@users.noreply.github.com>
2024-02-28 19:24:31 +0500

    Update ObstacleAssault.Build.cs

--- ObstacleAssault.Build.cs
+++ ObstacleAssault.Build.cs
@@ -7,9 +7,9 @@ public class ObstacleAssault : ModuleRules
 	public ObstacleAssault(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
+	//Need to add GAS modules in order to make sure that GAS has been implemented
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-
+//Same goes with private dependency module as well we need to add GAS Modules
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
